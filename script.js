8024cac718126ad496dc9dbeff0eec88e5c841a2
iamoen <owen.corona6@gmail.com>
2022-11-21 20:29:46 +0800

    second panel added

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-//1. start inputting number to textfield by clicking the buttons
-//2. next program the operands
-//      by clicking any operands, it needs to capture the current value
-//      on the text field and calculate to next input
-
-//psuedocode
-//user enters number/s
-//user select operand
-//text field capture the current value and resets the text field
-//user enter number/s
-//clicking equals solves the equation
-
-//calculate(n1, operands, n2)
-//switch (operands) case + - * 
 let memory = [];
 
 function getPanelValue(){
